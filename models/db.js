e86218dbb0a969cbc04fba06667517345c244f77
ATAGWE ROGER <atagweroger2@gmail.com>
2025-08-04 14:17:57 +0100

    Debugging

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || "postgresql://postgres:vGLdIIzujWSHlohlqGQavOONmVdKlIrG@shortline.proxy.rlwy.net:48453/railway",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Required for Railway SSL
   },
