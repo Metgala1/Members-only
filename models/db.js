049d172a3f31d5ce712c4a8570cb4b662b1aa36e
ATAGWE ROGER <atagweroger2@gmail.com>
2025-08-04 11:50:42 +0100

    Debug commit

--- models/db.js
+++ models/db.js
@@ -1,11 +1,12 @@
-const {Pool} = require("pg");
-const path = require("path")
-require("dotenv").config({
-  path: path.resolve(__dirname, "..", ".env"),
-});
-module.exports = new Pool({
-    connectionString: process.env.DATABASE_PUBLIC_URL,
-    ssl: {
-    rejectUnauthorized: false,
+const { Pool } = require('pg');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_PUBLIC_URL,
+  ssl: {
+    rejectUnauthorized: false, // Required for Railway SSL
   },
-})
+});
+
+module.exports = pool;
